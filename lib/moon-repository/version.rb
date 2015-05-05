8b2ca5950a5646cc179abe6af3922043747d2116
Corey Powell <mistdragon100@gmail.com>
2015-05-05 16:02:39 -0500

    Added Version docs!?

--- lib/moon-repository/version.rb
+++ lib/moon-repository/version.rb
@@ -1,9 +1,13 @@
 module Moon
   class Repository
+    # Version module
     module Version
+      # @return [Integer, nil]
       MAJOR, MINOR, TEENY, PATCH = 1, 1, 0, nil
+      # @return [String]
       STRING = [MAJOR, MINOR, TEENY, PATCH].compact.join('.')
     end
+    # @return [String]
     VERSION = Version::STRING
   end
 end
