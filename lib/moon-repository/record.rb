af14d01c3514eb3070f9e84c7fcc7286e4a73010
Corey Powell <mistdragon100@gmail.com>
2015-05-05 16:02:14 -0500

    Added Record docs

--- lib/moon-repository/record.rb
+++ lib/moon-repository/record.rb
@@ -1,25 +1,39 @@
 require 'moon-repository/repository'
 
 module Moon
-  # define a #repository method on your target
+  # Public facing api for users
   module Record
+    # Error raised when a record could not be found.
     class RecordNotFound < IndexError
     end
 
+    # Class extensions, this defines queries and creation methods
     module ClassMethods
-      # @return [Hash<Symbol, Object>]
+      # Repository configuration, only 2 options are supported at the moment,
+      # if the `:memory` is set to true, it will create a {Storage::Memory},
+      # for the repository, else a {Storage::YAMLStorage} is created instead.
+      #
+      # @return [Hash<Symbol, Object>] options
+      # @option options [Boolean] :memory  use an in memory storage?
+      # @option options [String] :filename  name for the YAMLStorage
       def repo_config
         {
           memory: true,
         }
       end
 
+      # Record class for instances
+      #
       # @return [Class]
       def model
         self
       end
 
-      def prepare_repository
+      # Called before creating the repository, use this method to
+      # create directories or preparations for the repo.
+      #
+      # @return [void]
+      private def prepare_repository
         #
       end
 
@@ -43,6 +57,8 @@ module Moon
         Repository.new(create_storage)
       end
 
+      # A Repository instance for storing records
+      #
       # @return [Repository]
       def repository
         @repository ||= begin
@@ -68,24 +84,32 @@ module Moon
         where({})
       end
 
-      def update_all(data, options = {})
-        where(options).each do |record|
+      # @param [Hash<Symbol, Object>] data
+      # @param [Hash<Symbol, Object>] query
+      def update_all(data, query = {})
+        where(query).each do |record|
           record.update(data)
         end
       end
 
-      def destroy_all(options = {})
-        where(options).each do |record|
+      # @param [Hash<Symbol, Object>] query
+      def destroy_all(query = {})
+        where(query).each do |record|
           record.destroy
         end
       end
 
-      def delete_all(options = {})
-        where(options).each do |record|
+      # @param [Hash<Symbol, Object>] query
+      def delete_all(query = {})
+        where(query).each do |record|
           repository.delete record.id
         end
       end
 
+      # Clears all records, this does not call destroy or delete, it simply
+      # deletes all the records from the repository
+      #
+      # @return [void]
       def clear_all
         repository.clear
       end
@@ -116,6 +140,7 @@ module Moon
         where(query).first
       end
 
+      # @param [Hash<Symbol, Object>] query
       def count(query = {})
         where(query).count
       end
@@ -129,13 +154,22 @@ module Moon
         model.new(repository.fetch(id))
       end
 
+      # Locates and returns the first record matching the query, if not
+      # record is found, raises a {RecordNotFound} error
+      #
+      # @param [Hash<Symbol, Object>] query
+      # @raise RecordNotFound
       def find_by(query)
         first(query) ||
           (raise RecordNotFound, "no record found for query: #{query}")
       end
     end
 
+    # Instance methods for interacting with Record instance objects
     module InstanceMethods
+      # The repository that corresponds with this model
+      #
+      # @return [Repository] The repository instance
       def repository
         self.class.repository
       end
@@ -149,37 +183,71 @@ module Moon
         to_h
       end
 
-      # Callback invoked when the model is created
+      # Callback invoked when the record is created in the repository
+      #
+      # @return [void]
+      # @abstract
       def on_create
       end
 
+      # Callback invoked before a record is updated in the repository
+      # see {#on_update}, {#update}
+      #
+      # @return [void]
+      # @abstract
       def pre_update
       end
 
-      # Callback invoked when the model is updated
+      # Callback invoked after the record is updated in the repository
+      # see {#pre_update}, {#update}
+      #
+      # @return [void]
+      # @abstract
       def on_update
       end
 
+      # Callback invoked after the record is updated or created in the repository.
+      # see {#on_save}, {#save}, {ClassMethods#create}, {#update}
+      #
+      # @return [void]
+      # @abstract
       def pre_save
       end
 
-      # General callback when an update or creation takes place.
+      # Callback invoked after the record is updated or created in the repository
+      # see {#pre_save}, {#save}, {ClassMethods#create}, {#update}
+      #
+      # @return [void]
+      # @abstract
       def on_save
       end
 
+      # Callback invoked before the record is destroyed in the repository
+      #
+      # @return [void]
+      # @abstract
       def pre_destroy
       end
 
-      # Callback invoked when the model is destroyed
+      # Callback invoked after the record is destroyed in the repository
+      #
+      # @return [void]
+      # @abstract
       def on_destroy
         @__destroyed = true
       end
 
+      # Updates the record's attributes/fields
+      #
       # @param [Hash<Symbol, Object>] data
+      # @return [void]
       def update_record(data)
         update_fields data
       end
 
+      # Updates the current record and invokes callbacks
+      #
+      # see {#pre_update}, {#on_update}
       # @param [Hash<Symbol, Object>] data
       # @return [self]
       def update(data)
@@ -191,6 +259,9 @@ module Moon
         self
       end
 
+      # Saves the current record and invokes callbacks
+      #
+      # see {#pre_save}, {#on_save}
       # @return [self]
       def save
         pre_save
@@ -199,6 +270,9 @@ module Moon
         self
       end
 
+      # Destroys the current record and invokes callbacks
+      #
+      # see {#pre_destroy}, {#on_destroy}
       # @return [self]
       def destroy
         pre_destroy
@@ -207,10 +281,19 @@ module Moon
         self
       end
 
+      # Reports if the record exists in the repository
+      #
+      # @return [Boolean] true if the record exists in the repository,
+      #                   false otherwise
       def exists?
         repository.exists?(id)
       end
 
+      # Reports if the model has been destroyed,
+      # @note, if this model existed before and another model was gotten
+      #        for its underlying data, it will report a false value
+      #
+      # @return [Boolean]
       def destroyed?
         @__destroyed
       end
@@ -218,6 +301,8 @@ module Moon
 
     include InstanceMethods
 
+    # @param [Module] mod
+    # @api private
     def self.included(mod)
       mod.extend ClassMethods
     end
